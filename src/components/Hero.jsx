b28e8da946a6909f75cccbbaedf53ae2b1f98ae1
UncomfortableChai <mj1055119@gmail.com>
2022-10-31 00:00:55 -0700

    added a scroll down image

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,6 +3,7 @@ import { motion } from 'framer-motion'
 import Logo from '../assets/images/JEM-Logo.svg'
 import ProPic from '../assets/images/ProfilePic.png'
 import MenuBtn from '../assets/images/Menu-btn-alt.svg'
+import Arrow from '../assets/images/Arrow.svg'
 import './hero.css'
 
 function Hero({ handleMouseEnter, handleMouseLeave, width }) {
@@ -17,53 +18,57 @@ function Hero({ handleMouseEnter, handleMouseLeave, width }) {
     //     }
     // }
 
+    // {width > 1020 ?
+    //     <motion.div
+    //         className='navbar'
+    //     // variants={variants}
+    //     // animate={ }
+    //     >
+    //         <a className='logo' href='https://www.thisworldthesedays.com/easter-egg4.html' target='_blank' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><img src={Logo} className="logo" alt="Logo" /></a>
+    //         <nav>
+    //             <ul>
+    //                 <a href='#' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><li>home</li></a>
+    //                 <a href='#' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><li>projects</li></a>
+    //                 <a href='#' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><li>about</li></a>
+    //                 {/* <a href='#' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><li>figma designs</li></a> */}
+    //                 <a href='#' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><li>contact</li></a>
+    //             </ul>
+    //         </nav>
+    //     </motion.div > :
+    //     <motion.div
+    //         className='navbar'
+    //     // variants={variants}
+    //     // animate={ }
+    //     >
+    //         <a className='logo' href='https://www.thisworldthesedays.com/easter-egg4.html' target='_blank' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><img src={Logo} className="logo" alt="Logo" /></a>
+    //         <button className='menu-btn'>
+    //             <img className='menu-svg' src={MenuBtn} alt="menu button" />
+    //         </button>
+    //         {/* <nav>
+    //         <ul>
+    //             <a href='#' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><li>home</li></a>
+    //             <a href='#' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><li>projects</li></a>
+    //             <a href='#' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><li>about</li></a>
+    //             {/* <a href='#' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><li>figma designs</li></a> */}
+    //         {/*<a href='#' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><li>contact</li></a>
+    //         </ul>
+    //     </nav> */}
+    //     </motion.div>
+    // }
+
     return (
         <div className="Hero-Section" >
             <div className='bg-Image'>
-                {width > 1020 ?
-                    <motion.div
-                        className='navbar'
-                    // variants={variants}
-                    // animate={ }
-                    >
-                        <a className='logo' href='https://www.thisworldthesedays.com/easter-egg4.html' target='_blank' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><img src={Logo} className="logo" alt="Logo" /></a>
-                        <nav>
-                            <ul>
-                                <a href='#' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><li>home</li></a>
-                                <a href='#' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><li>projects</li></a>
-                                <a href='#' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><li>about</li></a>
-                                {/* <a href='#' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><li>figma designs</li></a> */}
-                                <a href='#' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><li>contact</li></a>
-                            </ul>
-                        </nav>
-                    </motion.div> :
-                    <motion.div
-                        className='navbar'
-                    // variants={variants}
-                    // animate={ }
-                    >
-                        <a className='logo' href='https://www.thisworldthesedays.com/easter-egg4.html' target='_blank' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><img src={Logo} className="logo" alt="Logo" /></a>
-                        <button className='menu-btn'>
-                            <img className='menu-svg' src={MenuBtn} alt="menu button" />
-                        </button>
-                        {/* <nav>
-                        <ul>
-                            <a href='#' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><li>home</li></a>
-                            <a href='#' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><li>projects</li></a>
-                            <a href='#' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><li>about</li></a>
-                            {/* <a href='#' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><li>figma designs</li></a> */}
-                        {/*<a href='#' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><li>contact</li></a>
-                        </ul>
-                    </nav> */}
-                    </motion.div>
-                }
-                <div className='hero-main'>
+                <div className='hero-main' >
                     <img src={ProPic} alt="Picture of Dev" />
                     <div className='hero-txt'>
                         <h1>Joshua E. Martinez</h1>
                         <p>Web Developer</p>
                     </div>
-                </div>
+                </div >
+            </div >
+            <div className='scrolldown'>
+                <img src={Arrow}></img>
             </div>
         </div >
     )
