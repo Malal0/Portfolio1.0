f0f90d2b9125e836bc8ec930f63e1009a21dc197
UncomfortableChai <mj1055119@gmail.com>
2022-10-30 21:43:27 -0700

    adjusted the contact css

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,6 +7,44 @@ import PasswordgeneratorPreview from '../assets/images/passwordgeneratorprev.png
 import './projects.css'
 
 function Projects({ handleMouseEnter, handleMouseLeave }) {
+
+    const projectsArr = [
+        {
+            name: 'loteria online',
+            image: 'loteriaprev.png',
+            githublink: 'https://github.com/Malal0/Loteria',
+            url: 'https://loteria-card-generator.netlify.app/'
+        },
+        {
+            name: 'faux gym',
+            image: 'FauxGymDesktopView.png',
+            githublink: 'https://github.com/Malal0/Faux-Gym',
+            url: 'https://faux-gym.netlify.app/'
+        },
+        {
+            name: 'the best password generator',
+            image: 'loteriaprev.png',
+            githublink: 'https://github.com/Malal0/PasswordGenerator',
+            url: 'https://bestpasswordgenerator.netlify.app/'
+        }
+
+    ]
+
+    const projects = projectsArr.map(item => {
+        return (
+            <div className='project'>
+                <div className='project-cta'>
+                    <h2>{item.name}</h2>
+                    <div className='cta-links'>
+                        <a href={item.githublink} target='_blank' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><img src={GithubIcon} className="logo-link" alt="Github link" /></a>
+                        <a href={item.url} target='_blank' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><img src={WeblinkIcon} className="logo-link" alt="Website link" /></a>
+                    </div>
+                </div>
+                <img src={`../assets/images/${item.image}`} className="preview" alt="loteria online preview" />
+            </div>
+        )
+    })
+
     return (
         <div className='Projects-Section'>
             <h3>projects</h3>
