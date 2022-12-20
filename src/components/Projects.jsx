15bab083eb33a3f4be82f72af59b07299a4ef761
UncomfortableChai <mj1055119@gmail.com>
2022-12-20 13:03:10 -0800

    Changed the Projects component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,19 +11,19 @@ function Projects({ handleMouseEnter, handleMouseLeave }) {
     const projectsArr = [
         {
             name: 'loteria online',
-            image: 'loteriaprev.png',
+            image: LoteriaPreview,
             githublink: 'https://github.com/Malal0/Loteria',
             url: 'https://loteria-card-generator.netlify.app/'
         },
         {
             name: 'faux gym',
-            image: 'FauxGymDesktopView.png',
+            image: FauxgymPreview,
             githublink: 'https://github.com/Malal0/Faux-Gym',
             url: 'https://faux-gym.netlify.app/'
         },
         {
             name: 'the best password generator',
-            image: 'loteriaprev.png',
+            image: PasswordgeneratorPreview,
             githublink: 'https://github.com/Malal0/PasswordGenerator',
             url: 'https://bestpasswordgenerator.netlify.app/'
         }
@@ -40,7 +40,7 @@ function Projects({ handleMouseEnter, handleMouseLeave }) {
                         <a href={item.url} target='_blank' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><img src={WeblinkIcon} className="logo-link" alt="Website link" /></a>
                     </div>
                 </div>
-                <img src={`../assets/images/${item.image}`} className="preview" alt="loteria online preview" />
+                <img src={item.image} className="preview" alt={`${item.name} preview`} />
             </div>
         )
     })
@@ -48,36 +48,7 @@ function Projects({ handleMouseEnter, handleMouseLeave }) {
     return (
         <div className='Projects-Section'>
             <h3>projects</h3>
-            <div className='project'>
-                <div className='project-cta'>
-                    <h2>loteria<br></br>online</h2>
-                    <div className='cta-links'>
-                        <a href='https://github.com/Malal0/Loteria' target='_blank' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><img src={GithubIcon} className="logo-link" alt="Github link" /></a>
-                        <a href='https://loteria-card-generator.netlify.app/' target='_blank' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><img src={WeblinkIcon} className="logo-link" alt="Website link" /></a>
-                    </div>
-                </div>
-                <img src={LoteriaPreview} className="preview" alt="loteria online preview" />
-            </div>
-            <div className='project'>
-                <div className='project-cta'>
-                    <h2>faux<br></br>gym</h2>
-                    <div className='cta-links'>
-                        <a href='https://github.com/Malal0/Faux-Gym' target='_blank' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><img src={GithubIcon} className="logo-link" alt="Github link" /></a>
-                        <a href='https://faux-gym.netlify.app/' target='_blank' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><img src={WeblinkIcon} className="logo-link" alt="Website link" /></a>
-                    </div>
-                </div>
-                <img src={FauxgymPreview} className="preview" alt="faux gym preview" />
-            </div>
-            <div className='project'>
-                <div className='project-cta'>
-                    <h2>the best<br></br>password<br></br>generator</h2>
-                    <div className='cta-links'>
-                        <a href='https://github.com/Malal0/PasswordGenerator' target='_blank' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><img src={GithubIcon} className="logo-link" alt="Github link" /></a>
-                        <a href='https://bestpasswordgenerator.netlify.app/' target='_blank' onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}><img src={WeblinkIcon} className="logo-link" alt="Website link" /></a>
-                    </div>
-                </div>
-                <img src={PasswordgeneratorPreview} className="preview" alt="the best password generator preview" />
-            </div>
+            {projects}
         </div>
     )
 }
