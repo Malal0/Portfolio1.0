bc665fd1a75087559e9cbd3e49e9bf8a445e5423
Malal0 <mj1055119@gmail.com>
2023-03-20 19:56:32 -0700

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { motion } from 'framer-motion'
 import Hero from './components/Hero'
-import Projects from './components/Proj'
+import Projects from './components/Projects'
 import Skills from './components/Skills'
 import AboutMe from './components/AboutMe'
 import Contact from './components/Contact'
