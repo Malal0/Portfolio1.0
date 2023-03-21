0573852c64a93a25bb017e9119c3e6b99319c40d
Malal0 <mj1055119@gmail.com>
2023-03-20 19:29:45 -0700

    updated projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+// import React, { useEffect } from 'react'
 import GithubIcon from '../assets/images/githubicon.svg'
 import WeblinkIcon from '../assets/images/weblinkicon.svg'
 import moviewatchlistbg from '../assets/images/moviewatchlistbg.png'
@@ -45,22 +45,22 @@ function Projects({ handleMouseEnter, handleMouseLeave }) {
         )
     })
 
-    const images = document.querySelectorAll('.preview');
+    // const images = document.querySelectorAll('.preview');
 
-    function handleImageEnter(e) {
-        e.target.previousSibling.firstChild.classList.add('bordered')
-        e.target.previousSibling.lastChild.classList.add('bordered')
-    }
+    // function handleImageEnter(e) {
+    //     e.target.previousSibling.firstChild.classList.add('bordered')
+    //     e.target.previousSibling.lastChild.classList.add('bordered')
+    // }
 
-    function handleImageLeave(e) {
-        e.target.previousSibling.firstChild.classList.remove('bordered')
-        e.target.previousSibling.lastChild.classList.remove('bordered')
-    }
+    // function handleImageLeave(e) {
+    //     e.target.previousSibling.firstChild.classList.remove('bordered')
+    //     e.target.previousSibling.lastChild.classList.remove('bordered')
+    // }
 
-    useEffect(() => {
-        images.forEach((image) => image.addEventListener('mouseenter', handleImageEnter));
-        images.forEach((image) => image.addEventListener('mouseleave', handleImageLeave));
-    }, []);
+    // useEffect(() => {
+    //     images.forEach((image) => image.addEventListener('mouseenter', handleImageEnter));
+    //     images.forEach((image) => image.addEventListener('mouseleave', handleImageLeave));
+    // }, []);
 
     return (
         <div className='Projects-Section'>
